1145cbc513bba4f1340dafda49c44e045dce874d
Anderson Oliveira <anderson.sfoliveira@gmail.com>
2022-09-23 13:02:58 -0300

    96. Monitorando nossa aplicação Spring boot com o Spring boot Admin

--- src/main/java/br/com/brlsistemas/adminapps/AdminAppsApplication.java
+++ src/main/java/br/com/brlsistemas/adminapps/AdminAppsApplication.java
@@ -1,9 +1,11 @@
 package br.com.brlsistemas.adminapps;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class AdminAppsApplication {
 
 	public static void main(String[] args) {

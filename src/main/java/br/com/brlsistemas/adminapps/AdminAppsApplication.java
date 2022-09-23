e1f548871aa25950eff7c1b0c1ea540d98231331
Anderson Oliveira <anderson.sfoliveira@gmail.com>
2022-09-23 11:23:17 -0300

    First commit

---
+++ src/main/java/br/com/brlsistemas/adminapps/AdminAppsApplication.java
@@ -0,0 +1,13 @@
+package br.com.brlsistemas.adminapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminAppsApplication.class, args);
+	}
+
+}
